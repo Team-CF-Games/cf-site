197dd046288243271ea0d3f79dd497ead7140a18
Lulu-76450 <creatif.france@outlook.com>
2025-06-14 17:58:04 +0200

    Create ping.js

---
+++ netlify/functions/ping.js
@@ -0,0 +1,13 @@
+const { exec } = require('child_process');
+
+exports.handler = async function(event, context) {
+  return new Promise((resolve, reject) => {
+    exec('ping -c 1 136.243.69.23', (error) => {
+      if (error) {
+        resolve({ statusCode: 500, body: 'DOWN' });
+      } else {
+        resolve({ statusCode: 200, body: 'OK' });
+      }
+    });
+  });
+};
